a47a76a9f922c9b635dff10b697ff07d32bb9658
Akash M <akashm006@outlook.com>
2024-07-31 18:44:56 +0530

    Added service

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,1 @@
+const apiBaseUrl = "https://dummy.restapiexample.com/api/v1";
---
+++ lib/src/features/employees/data/data_sources/remote/employee_api_service.dart
@@ -0,0 +1,14 @@
+import 'package:http_requests/src/constants/constants.dart';
+import 'package:http_requests/src/features/employees/data/models/employee.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'employee_api_service.g.dart';
+
+@RestApi(baseUrl: apiBaseUrl)
+abstract class EmployeeApiService {
+  factory EmployeeApiService(Dio dio) = _EmployeeApiService;
+
+  @GET('/employees')
+  Future<HttpResponse<List<EmployeeModel>>> getEmployees();
+}
